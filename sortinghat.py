3add1e56236d439c7e3dcfa017bd2cbce52374c6
Trinav Prasad Talukdar <144201740+Trinav-PT@users.noreply.github.com>
2025-09-23 01:51:32 +0530

    Update sortinghat.py

--- sortinghat.py
+++ sortinghat.py
@@ -294,7 +294,7 @@ if st.checkbox("Show House Champions & Statistics"):
         )
         
         # House Champions
-        st.markdown("<h3 style='color:#3e2723; font-family: Georgia;'>Most Dedicated House Members:</h3>", unsafe_allow_html=True)
+        st.markdown("<h3 style='color:#3e2723; font-family: Georgia;'>These People Embody Their House The Most:</h3>", unsafe_allow_html=True)
         for house in HOUSES:
             champion = champions.get(house, "None")
             st.markdown(f"<p style='font-size:18px; color:#3e2723;'><strong>Most {house}:</strong> {champion}</p>", unsafe_allow_html=True)
