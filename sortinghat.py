0f5e0a3921c669a791f811ea8356d9209d4396ac
Trinav Prasad Talukdar <144201740+Trinav-PT@users.noreply.github.com>
2025-09-24 01:36:14 +0530

    Update sortinghat.py

--- sortinghat.py
+++ sortinghat.py
@@ -646,7 +646,7 @@ st.markdown(
                 <p class="credit-name">Manaasve</p>
             </div>
             <div class="credit-line">
-                <p class="credit-role">Mischief Managed. See you Monday =)</p>
+                <p class="credit-role">Mischief Managed. See you on Monday =)</p>
             </div>
         </div>
     </div>
