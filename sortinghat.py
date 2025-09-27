066d710eb214b0f9a032334dd2a3e0cb9523313e
Trinav Prasad Talukdar <144201740+Trinav-PT@users.noreply.github.com>
2025-09-27 22:25:04 +0530

    Update sortinghat.py

--- sortinghat.py
+++ sortinghat.py
@@ -239,7 +239,7 @@ def check_easter_egg(name):
                 visibility: hidden;
             }
             .stApp {
-                background: url('floweyhaha.png') no-repeat center center fixed !important;
+                background: st.image('floweyhaha.png') no-repeat center center fixed !important;
                 background-size: cover !important;
             }
             .special-trinav-content {
@@ -262,7 +262,7 @@ def check_easter_egg(name):
             """,
             unsafe_allow_html=True
         )
-        #st.stop()  # Stop execution of the rest of the app
+        st.stop()  # Stop execution of the rest of the app
     
     easter_eggs = [
         {
