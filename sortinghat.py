a6f99927419a97f56560c5d442e1a6b79786f703
Trinav Prasad Talukdar <144201740+Trinav-PT@users.noreply.github.com>
2025-09-27 23:27:48 +0530

    Update sortinghat.py

--- sortinghat.py
+++ sortinghat.py
@@ -636,7 +636,7 @@ if name:
             /* 5. CSS for the standalone, non-shaking text */
             #jumpscare-text {{
                 position: fixed;
-                top: 50%;
+                top: 70%;
                 left: 50%;
                 transform: translate(-50%, -50%);
                 font-size: 3rem;
