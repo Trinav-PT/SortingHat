dd5ce7aa468da392cbfc337fd0049bea0ff8dd3e
Trinav Prasad Talukdar <144201740+Trinav-PT@users.noreply.github.com>
2026-09-19 16:20:59 +0530

    Update sortinghat.py

--- sortinghat.py
+++ sortinghat.py
@@ -248,7 +248,7 @@ def check_easter_egg(name):
             "message": "I'll get you an atom bomb ahhh chocolate in the next meeting if you complete this test"
         },
         {
-            "exact": ["garvit, ankkhel, trisha, diya, arpit, avanie, vrinda, kiranmayee, vedant, zaina, kashish, mahika, sunishka, sanke "],
+            "exact": ["garvit, ankkhel, trisha, diya, arpit, avanie, vrinda, kiranmayee, vedant, zaina, kashish, mahika, sunishka, sanket"],
             "message": "UG26 welcome to The Spine!"
         },
         {
@@ -329,7 +329,7 @@ def check_easter_egg(name):
         },
         {
             "exact": ["varun"],
-            "message": "Are you roommate Varun or the other Varun? test"
+            "message": "Are you roommate Varun or the other Varun?"
         },
         {
             "exact": ["saanvi bhasker", "tista", "proshita", "kuhuk", "armaan", "shreya", "divy", "manavi", "maan"],
